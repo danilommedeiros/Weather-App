44a95e4cb4b3c17f7e884e3b471426a72bd8156e
Danilo Medeiros <danilo.medeiros801@gmail.com>
2021-10-27 00:17:20 -0300

    final

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".city").innerText = "Clima em " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    document.querySelector(".humidity").innerText = "Humidade: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Velocidade do vento: " + speed + " km/h";
     document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
   },
   search: function () {
