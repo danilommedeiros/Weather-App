59063f67ef46d7953035281af79222e0e14fdfd9
Danilo Medeiros <danilo.medeiros801@gmail.com>
2021-10-26 21:07:09 -0300

    all

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 let weather = {
-    "apiKey": "d3fd379167ffb1424ae70a84ad9e4c9d"
-}
+    "apiKey": "d3fd379167ffb1424ae70a84ad9e4c9d",
+   fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+  displayWeather: function(data) {
+      const { name } = data;
+      const { icon, description } = data.weather;
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      console.log("name,icon,description,temp,humidity,speed")
+    }
+};
