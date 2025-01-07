8eda0702e3227ba0dc77530ca49d5c841200155e
Danilo Medeiros <63961886+danilommedeiros@users.noreply.github.com>
2025-01-07 15:09:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 let weather = {
   apiKey: "d3fd379167ffb1424ae70a84ad9e4c9d",
-  unsplashAccessKey: "SUA_UNSPLASH_ACCESS_KEY", // Adicione sua chave da API Unsplash
-
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -20,7 +18,7 @@ let weather = {
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     const { temp_max, temp_min } = data.main;
-    document.querySelector("#city").innerText = "Clima em " + ( name + ", " + country );
+    document.querySelector("#city").innerText = "Clima em " + ( name + "," + " " + country );
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + "@2x.png";
     document.querySelector(".description").innerText = description;
@@ -35,21 +33,8 @@ let weather = {
       "Humidade: " + humidity + "%";
     document.querySelector("#wind").innerText =
       "Velocidade do vento: " + speed + " km/h";
-
-    // Busca imagem da cidade usando a API da Unsplash com dimensões específicas
-    fetch(
-      `https://api.unsplash.com/photos/random?query=${name},city&orientation=landscape&w=1600&h=900&client_id=${this.unsplashAccessKey}`
-    )
-      .then((response) => response.json())
-      .then((imageData) => {
-        const imageUrl = imageData.urls.full;
-        document.body.style.backgroundImage = `url('${imageUrl}')`;
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundPosition = 'center';
-      })
-      .catch(() => {
-        document.body.style.backgroundImage = "url('default-background.jpg')";
-      });
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
@@ -67,3 +52,6 @@ document
       weather.search();
     }
   });
+
+
+
